1e936cbc04d2276b334c18f7e0adf2bac1711a9b
dlozaco975@gmail.com <dlozaco975@gmail.com>
2024-12-29 00:47:58 +0100

    Added MVP per match with his team and top3 goals + assistences needs to be fixed

--- db/queriesDB.sql
+++ db/queriesDB.sql
@@ -17,7 +17,7 @@ LIMIT 3;
 
 
 -- Top 3 people with the most goals + assistences
-CREATE OR REPLACE VIEW goalsPlayers AS
+CREATE OR REPLACE VIEW goalsPlayers AS 
 	SELECT p.idPlayer, COUNT(*) AS goal
 	FROM players p
 	JOIN goals g ON p.idPlayer = g.idGoalPlayer
@@ -31,9 +31,16 @@ CREATE OR REPLACE VIEW assistencePlayers AS
 	GROUP BY (p.name)
 	ORDER BY COUNT(*) DESC;
 
-SELECT p.name, gp.goal + ap.assistence
+SELECT p.name, gp.goal + ap.assistence AS gpAp
 FROM players p
-JOIN goalsplayers gp ON gp.idPlayer = p.idPlayer
-JOIN assistenceplayers ap ON ap.idPlayer = p.idPlayer
+LEFT JOIN goalsplayers gp ON gp.idPlayer = p.idPlayer
+LEFT JOIN assistenceplayers ap ON ap.idPlayer = p.idPlayer
+ORDER BY gpAp DESC
 LIMIT 3;
 
+-- MVP of each game and his team
+SELECT m.idMMatch, m.date_match, p.name, pt.idTeam
+FROM mmatchs m
+JOIN players p ON m.idMVP = p.idPlayer
+JOIN teamsplayers pt ON p.idPlayer = pt.idPlayer
+WHERE pt.idTeam = m.idTeam1 OR pt.idTeam = m.idTeam2
